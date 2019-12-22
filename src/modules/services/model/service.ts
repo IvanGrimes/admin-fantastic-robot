6d397c0d099e3f11460e1c32402b19f7de51f779
Ivan Grimes <ivan@grimesco.de>
2019-12-22 09:05:22 +0300

    configure service

--- src/modules/services/model/service.ts
+++ src/modules/services/model/service.ts
@@ -1,3 +1,25 @@
 import defaultAxios from 'axios';
+import { paramsSerializer } from '../utils/paramsSerializer';
 
-export const service = defaultAxios.create();
+if (!process.env.API_AUTH_USER || !process.env.API_AUTH_PASSWORD) {
+  throw new Error('You should define API_AUTH_USER and API_AUTH_PASSWORD');
+}
+
+export const axiosClient = defaultAxios.create({
+  baseURL: process.env.API_ENDPOINT,
+  auth: {
+    username: process.env.API_AUTH_USER,
+    password: process.env.API_AUTH_PASSWORD,
+  },
+  paramsSerializer,
+});
+
+axiosClient.interceptors.request.use(config => {
+  const nextConfig = config;
+
+  if (nextConfig.url) {
+    nextConfig.url = nextConfig.url.replace('/api', '');
+  }
+
+  return nextConfig;
+});
